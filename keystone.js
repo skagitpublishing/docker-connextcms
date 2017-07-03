adfde481b1120b9ad8e159ccb2be0cc3364eae33
Chris Troutner <chris.troutner@gmail.com>
2017-07-03 14:40:16 -0700

    Switching from port 3000 to port 80

--- keystone.js
+++ keystone.js
@@ -23,7 +23,7 @@ keystone.init({
   'favicon': 'public/favicon.ico',
   'views': 'templates/views',
   'view engine': 'hbs',
-  'port': 80,
+  'port': 3000,
 
   'custom engine': handlebars.create({
           layoutsDir: 'templates/views/layouts',
