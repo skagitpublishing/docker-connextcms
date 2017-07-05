ad76cfde65d2896827b3cfb81b09760e7188df53
Chris Troutner <chris.troutner@gmail.com>
2017-07-05 08:35:56 -0700

    Giving node.js permission to run on port 80

--- keystone.js
+++ keystone.js
@@ -23,7 +23,7 @@ keystone.init({
   'favicon': 'public/favicon.ico',
   'views': 'templates/views',
   'view engine': 'hbs',
-  'port': 3000,
+  'port': 80,
 
   'custom engine': handlebars.create({
           layoutsDir: 'templates/views/layouts',
